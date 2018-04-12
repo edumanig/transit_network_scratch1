8c0910edd33b8cb6acd2853b8035b971067253b6
Ubuntu <ubuntu@ip-172-23-0-216.us-west-1.compute.internal>
2018-04-12 06:10:10 +0000

    run in 2nd controller 52.52.3.186.secret.tfvars

--- apply.sh
+++ apply.sh
@@ -2,6 +2,6 @@
 HOME=/home/ubuntu
 
 terraform init
-terraform validate -var-file=$HOME/13.57.159.116.secret.tfvars
-terraform plan -var-file=$HOME/13.57.159.116.secret.tfvars
-#terraform apply -auto-approve -var-file=$HOME/13.57.159.116.secret.tfvars
+terraform validate -var-file=$HOME/52.52.3.186.secret.tfvars
+terraform plan -var-file=$HOME/52.52.3.186.secret.tfvars
+terraform apply -auto-approve -var-file=$HOME/52.52.3.186.secret.tfvars
